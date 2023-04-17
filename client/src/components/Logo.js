e26f80abf6c366f354ded2f2c1b5008dee0e9b0f
omnaladkar <75687080+omnaladkar@users.noreply.github.com>
2023-04-17 23:08:07 +0530

    Initial commit

---
+++ client/src/components/Logo.js
@@ -0,0 +1,9 @@
+import logo from '../assets/images/logo2.png'
+
+const Logo = () => {
+  return <img style ={{
+    width:"200px"
+  }}src={logo} alt='jobify' className='logo' />
+}
+
+export default Logo
